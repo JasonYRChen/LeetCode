b04b112d070b063a0877d512fa6a7454e7951988
JasonYRChen <jkmylife@gmail.com>
2021-06-23 23:28:58 +0800

    refinement

--- Add_two_numbers_2.py
+++ Add_two_numbers_2.py
@@ -7,7 +7,7 @@ class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
         temp, head = 0, ListNode()
         node = head
-        while l1 or l2:
+        while l1 or l2 or temp:
             n1 = l1.val if l1 else 0
             n2 = l2.val if l2 else 0
             next_node = ListNode((n1 + n2 + temp) % 10)
@@ -15,7 +15,5 @@ class Solution:
             node.next, node = next_node, next_node
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-        if temp:
-            node.next = ListNode(1)
         return head.next
 
