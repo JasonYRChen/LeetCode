d2811b860f0727f1be01c1873c9ebd3f983124b0
JasonYRChen <jkmylife@gmail.com>
2021-06-27 11:06:03 +0800

    6. ZigZag Conversion: add new solution

--- ZigZag_Conversion_6.py
+++ ZigZag_Conversion_6.py
@@ -1,5 +1,6 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        # sort char in s into numRows part
         words = [''] * numRows
         cycle = 2*numRows-2 if 2*numRows-2 else 1
         for i, char in enumerate(s):
@@ -8,4 +9,20 @@ class Solution:
                 row = cycle - row
             words[row] += char
         return ''.join(words)
+    
+    # find the char and append right to the next char in word
+    # def convert(self, s: str, numRows: int) -> str:
+    #     word = ''
+    #     section = 2 * numRows - 2  if 2*numRows-2 else 1
+    #     start = 0
+    #     idx, left, right = 0, 0, section
+    #     for _ in range(len(s)):
+    #         word += s[idx]
+    #         idx += left if idx % section >= numRows else right
+    #         if idx >= len(s):
+    #             start -= 1
+    #             left = 2 * abs(start)
+    #             right = section - left if left < section else section
+    #             idx = start + left
+    #     return word
 
