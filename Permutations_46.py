54e87cafb1ca6280f57edd186eef9d1093b90737
JasonYRChen <jkmylife@gmail.com>
2021-07-10 19:30:39 +0800

    46. Permutations (add new and faster solution)

--- Permutations_46.py
+++ Permutations_46.py
@@ -1,22 +1,34 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        result = [nums]
-        for _ in range(factorial(len(nums)) - 1):
-            new = result[-1].copy()
-            for anchor in range(len(new)-2, -1, -1):
-                if new[anchor] < new[anchor+1]:
-                    break
-            for exchange in range(anchor+1, len(new)):
-                if new[exchange] < new[anchor]:
-                    exchange -= 1
-                    break
-            new[anchor], new[exchange] = new[exchange], new[anchor]
-            left, right = anchor + 1, len(new) - 1
-            while left < right:
-                new[left], new[right] = new[right], new[left]
-                left += 1
-                right -= 1
-            result.append(new)
-        return result
+        return list(self.yield_seq(nums, []))
+        
+    def yield_seq(self, nums, saving):
+        if not nums:
+            yield saving
+        else:
+            for i, num in enumerate(nums):
+                yield from self.yield_seq(nums[:i]+nums[i+1:], saving+[num])
+
+
+## Solution 2: always find the next lexicographically larger sequence.
+#    def permute(self, nums: List[int]) -> List[List[int]]:
+#        nums.sort()
+#        result = [nums]
+#        for _ in range(factorial(len(nums)) - 1):
+#            new = result[-1].copy()
+#            for anchor in range(len(new)-2, -1, -1):
+#                if new[anchor] < new[anchor+1]:
+#                    break
+#            for exchange in range(anchor+1, len(new)):
+#                if new[exchange] < new[anchor]:
+#                    exchange -= 1
+#                    break
+#            new[anchor], new[exchange] = new[exchange], new[anchor]
+#            left, right = anchor + 1, len(new) - 1
+#            while left < right:
+#                new[left], new[right] = new[right], new[left]
+#                left += 1
+#                right -= 1
+#            result.append(new)
+#        return result
                     
