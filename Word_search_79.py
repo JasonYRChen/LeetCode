f526148bb56057f614132d8de78b3534dd45080e
JasonYRChen <jkmylife@gmail.com>
2021-07-26 14:30:06 +0800

    79. Word search (add new solution)

--- Word_search_79.py
+++ Word_search_79.py
@@ -1,5 +1,36 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        counter = defaultdict(int)
+        for r in board:
+            for c in r:
+                counter[c] += 1
+        for c in word:
+            if counter[c] == 0:
+                return False
+            counter[c] -= 1
+        
+        for row in range(len(board)):
+            for col in range(len(board[0])):
+                if board[row][col] == word[0] and self.check_exist(board, word[1:], set(), row, col):
+                    return True
+        return False
+    
+    def check_exist(self, board, word, used, row, col):
+        if not word:
+            return True
+        used.add((row, col))
+        abandoned = {(-1, col), (len(board), col), (row, -1), (row, len(board[0]))}
+        candidates = {(row-1, col), (row+1, col), (row, col-1), (row, col+1)} - used - abandoned
+        for r, c in candidates:
+            if board[r][c] == word[0] and self.check_exist(board, word[1:], used, r, c):
+                return True
+            used -= {(r, c)}
+
+
+
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
         c_board, chars = defaultdict(int), defaultdict(set)
         for row in range(len(board)):
             for col in range(len(board[0])):
