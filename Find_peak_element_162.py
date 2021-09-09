c83f37dfb3cba73c3a196e2441394bba77498318
JasonYRChen <jkmylife@gmail.com>
2021-09-09 15:21:59 +0800

    162. Find Peak Element

---
+++ Find_peak_element_162.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findPeakElement(self, nums) -> int:
+        left, right = 0, len(nums) - 1
+        while right > left:
+            mid = (left + right) // 2
+            if nums[mid] > nums[mid+1]:
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+
+if __name__ == '__main__':
+    n = [10, 8, 5, 6, 7, 9] 
+    print(Solution().findPeakElement(n))
+
