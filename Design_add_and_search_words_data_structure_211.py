5a734ba5316998130926ae4f68288852220175ed
JasonYRChen <jkmylife@gmail.com>
2021-09-30 23:10:42 +0800

    211. Design Add and Search Words Data Structure (2 solutions)

--- Design_add_and_search_words_data_structure_211.py
+++ Design_add_and_search_words_data_structure_211.py
@@ -1,8 +1,30 @@
+from collections import defaultdict
+
+
+class WordDictionary:
+    def __init__(self):
+        self.data = defaultdict(set)
+
+    def addWord(self, word: str) -> None:
+        self.data[len(word)].add(word)
+
+    def search(self, word: str) -> bool:
+        for compare in self.data[len(word)]:
+            for c, w in zip(compare, word):
+                if w != '.' and w != c:
+                    break
+            else:
+                return True
+        return False
+
+
+# A dfs solution
 class WordDictionary:
     def __init__(self):
         self.data = {}
 
     def addWord(self, word: str) -> None:
+        word = word[::-1]
         data = self.data
         for char in word:
             if char not in data:
@@ -11,7 +33,7 @@ class WordDictionary:
         data['#'] = '#'
 
     def search(self, word: str) -> bool:
-        return self.dfs(self.data, word)
+        return self.dfs(self.data, word[::-1])
 
     def dfs(self, data, word):
         if not word:
