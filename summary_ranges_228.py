46e706efdcf6e70bb65ae10cd0011e490dc8aca1
JasonYRChen <jkmylife@gmail.com>
2023-06-06 07:52:27 +0800

    228. Summary Ranges

---
+++ summary_ranges_228.py
@@ -0,0 +1,14 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        ans = []
+        start = 0
+        while start < len(nums):
+            end = start + 1
+            while (end < len(nums)) and (nums[end] - nums[start] == end - start):
+                end += 1
+            if start == end - 1:
+                ans.append(str(nums[start]))
+            else:
+                ans.append(f'{nums[start]}->{nums[end-1]}')
+            start = end
+        return ans
