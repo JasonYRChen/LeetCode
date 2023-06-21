917bc0bf91d955742df533e4d5a988635d08cb29
JasonYRChen <jkmylife@gmail.com>
2023-06-22 06:10:45 +0800

    287. refine the speed

--- find_the_duplicate_number_287.py
+++ find_the_duplicate_number_287.py
@@ -2,7 +2,6 @@ class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         counter = defaultdict(int)
         for num in nums:
+            if counter[num]:
+                return num
             counter[num] += 1
-        for k, v in counter.items():
-            if v != 1:
-                return k
