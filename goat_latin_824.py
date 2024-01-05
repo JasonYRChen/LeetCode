3cce9872a8f3464aeb77bddeb9a6323e04055739
JasonYRChen <jkmylife@gmail.com>
2024-01-06 06:20:49 +0800

    824. Goat Latin

---
+++ goat_latin_824.py
@@ -0,0 +1,9 @@
+class Solution:
+    def toGoatLatin(self, sentence: str) -> str:
+        vowel = {'a', 'e', 'i', 'o', 'u'}
+        words = sentence.split()
+        for i in range(len(words)):
+            if words[i][0].lower() not in vowel:
+                words[i] = words[i][1:] + words[i][0]
+            words[i] += 'ma' + 'a' * (i + 1)
+        return ' '.join(words)
