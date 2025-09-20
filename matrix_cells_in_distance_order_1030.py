162c098942b031409069d336c0ffae01a5249ffd
JasonYRChen <jkmylife@gmail.com>
2025-09-21 06:26:22 +0800

    1030. Matrix Cells in Distance Order

---
+++ matrix_cells_in_distance_order_1030.py
@@ -0,0 +1,5 @@
+class Solution:
+    def allCellsDistOrder(self, rows: int, cols: int, rCenter: int, cCenter: int) -> List[List[int]]:
+        matrix = [(i, j) for i in range(rows) for j in range(cols)]
+        matrix.sort(key=lambda x: abs(x[0]-rCenter) + abs(x[1]-cCenter))
+        return matrix
